7a3d7fec0019bb8e4b7688d105fd5ed1c2b52b0c
svc-student <svc-student@student.directory.jccc>
2016-04-14 17:21:30 -0500

    Commit
    Commit

---
+++ Assets/Video6/triggers.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggers : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("Coin Collected");
+        Destroy(gameObject);
+    }
+}
